0ce34f5ebf096f5b394c1331bbedfdf2be15d768
Krishna <krishnavamsin202@gmail.com>
2024-11-21 17:32:30 +0530

    Removed

--- client/src/components/Loaders/dotLoader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Loading from "./loading_gif.gif"
-const DotLoader = () => {
-    return (
-        <img src={Loading} alt='dot-loader' height={50} width={50} />
-    )
-}
-
-export default DotLoader
--- client/src/components/NoData/NoData.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Image from "./NoDoc.svg";
-const NoData = () => {
-    return (
-        <img src={Image} alt='no doctors' />
-    )
-}
-
-export default NoData;
